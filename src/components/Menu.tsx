ca2a41a65e3611b63c323390ceb453a13663d621
tomghaines <tomhaines001@gmail.com>
2024-08-09 13:10:24 +0100

    icons added to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,13 @@
 import MenuBtn from './MenuBtn'
 
 const Menu = () => {
+  const airplaneTiltIcon = '../src/assets/icons/AirplaneTilt.svg'
+  const cloudSunIcon = '../src/assets/icons/CloudSun.svg'
+  const graphIcon = '../src/assets/icons/Graph.svg'
+  const stackIcon = '../src/assets/icons/Stack.svg'
+  const magnifyingGlassIcon = '../src/assets/icons/MagnifyingGlass.svg'
+  const fadersHorizontalIcon = '../src/assets/icons/FadersHorizontal.svg'
+
   return (
     <div className="bg-[#17171795] p-4 justify-center flex flex-col w-96 items-stretch rounded-2xl m-4 backdrop-blur-lg">
       <div className="flex justify-between mb-4">
@@ -15,26 +22,40 @@ const Menu = () => {
       </div>
 
       <div className="w-auto flex flex-col gap-4">
-        <label htmlFor="search-inp"></label>
-        <input
-          name="search-inp"
-          id="search-inp"
-          className="bg-[#00000050] border border-[#FFFFFF10] focus:border-[#FFFFFF25] focus:outline-none rounded-lg p-3 placeholder-gray-500 font-thin"
-          type="text"
-          placeholder="Search flights, airports, and more"
-        />
+        <label htmlFor="search-inp" className="sr-only"></label>
+        <div className="relative">
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <img
+              src={magnifyingGlassIcon}
+              alt="Search Icon"
+              className="text-gray-500"
+            />
+          </span>
+          <input
+            name="search-inp"
+            id="search-inp"
+            className="bg-[#00000050] w-full border border-[#FFFFFF10] focus:border-[#FFFFFF25] focus:outline-none rounded-lg p-3 pl-10 placeholder-gray-500 font-thin"
+            type="text"
+            placeholder="Search flights, airports, and more"
+          />
+        </div>
         <MenuBtn
-          icon="i"
+          icon={airplaneTiltIcon}
           title="Most tracked Flights"
           isLiveSVG="i"
-          isLiveTxt="LIVE"
+          isLiveTxt=""
         />
-        <MenuBtn icon="i" title="Statistics" isLiveSVG="i" isLiveTxt="LIVE" />
-        <MenuBtn icon="i" title="Weather" />
-        <MenuBtn icon="i" title="Filters" />
+        <MenuBtn
+          icon={graphIcon}
+          title="Statistics"
+          isLiveSVG="i"
+          isLiveTxt=""
+        />
+        <MenuBtn icon={cloudSunIcon} title="Weather" />
+        <MenuBtn icon={stackIcon} title="Filters" />
       </div>
       <div className="w-full">
-        <MenuBtn icon="i" title="Settings" />
+        <MenuBtn icon={fadersHorizontalIcon} title="Settings" />
       </div>
     </div>
   )
--- src/components/MenuBtn.tsx
+++ src/components/MenuBtn.tsx
@@ -1,21 +1,31 @@
 import React from 'react'
 
 interface Props {
-  icon: React.ReactNode
+  icon: React.ReactNode | string
   title: string
-  isLiveSVG?: React.ReactNode
+  isLiveSVG?: string
   isLiveTxt?: string
 }
 
 const MenuBtn = ({ icon, title, isLiveSVG, isLiveTxt }: Props) => {
   return (
     <div className="gap-3 flex p-3 rounded-lg bg-[#151515]">
-      <div>{icon}</div>
-      <span className="text-white">{title}</span>
-      <div className="flex">
-        <div>{isLiveSVG}</div>
-        <span className="text-white">{isLiveTxt}</span>
+      <div>
+        {typeof icon === 'string' ? (
+          <img src={icon} alt={`${title} Icon`} />
+        ) : (
+          icon
+        )}
       </div>
+      <span className="text-white">{title}</span>
+      {isLiveSVG && isLiveTxt && (
+        <div className="flex">
+          <div>
+            <img src={isLiveSVG} alt="Live Indicator" />
+          </div>
+          <span className="text-white">{isLiveTxt}</span>
+        </div>
+      )}
     </div>
   )
 }
